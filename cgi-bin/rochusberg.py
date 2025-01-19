909de896c91b70a21b2dd32957a31473c27c87cc
Markus Vieth <vieth@uni-mainz.de>
2025-01-19 17:41:05 +0100

    Add Rochusberg

---
+++ cgi-bin/rochusberg.py
@@ -0,0 +1,18 @@
+import re
+from print_xml import print_xml
+
+# Ausgabe Rochusberg = Hauptspeise
+# Y_Eintopf = Eintopf
+RENUMS = re.compile(r" \([^\)]*\)")
+
+URL = "https://www.studierendenwerk-mainz.de/speiseplan/Speiseplan.xml"
+VERSION = "1.0.0"
+VERBRAUCHSORT = "431"
+IGNORE = ["Ausgabe intern ZM", "Ausgabe intern ZM vegan"]
+
+CATEGORY = {
+	"Ausgabe Rochusberg": "Hauptspeise",
+	"Y_Eintopf": "Eintopf",
+}
+
+print_xml(VERBRAUCHSORT, CATEGORY, URL, VERSION, RENUMS, IGNORE)
